4fad8a7542384ba06132f4a7cda2310f6f1f6c35
Naiem Islam <naiemislam1547@gmail.com>
2023-05-01 10:29:53 +0600

    add button updated

--- js/app.js
+++ js/app.js
@@ -1,5 +1,36 @@
 document.getElementById("detail-submit-btn").addEventListener('click', function(){
-    const userDetailsInputValue = document.getElementById('buyer-details-input').value;
-    document.getElementById("buyer-info").innerText = userDetailsInputValue;
+    const userDetailsInputValue = document.getElementById('buyer-details-input');
+    document.getElementById("buyer-info").innerText = userDetailsInputValue.value;
+    userDetailsInputValue.value = "";
+
+});
+
+const addProductsBtn = document.getElementById("add-details-btn");
+
+addProductsBtn.addEventListener('click', function(){
+
+    const infoTable = document.getElementById('info-table');
+    const itemName = document.getElementById('item-name-input');
+    const itemPrice = document.getElementById('item-price-input');
+    const itemQuantity = document.getElementById('item-quantity-input');
+
+    // create dynamic table jekhane uporeer info gula dekhabe haaa
+    const totalPrice = parseInt(itemPrice.value) * parseInt(itemQuantity.value);
+    const tr = document.createElement('tr');
+    const th = document.createElement('th');
+    const td1 = document.createElement('td');
+    const td2 = document.createElement('td');
+    const td3 = document.createElement('td');
+
+    th.innerText = itemName.value;
+    td1.innerText = itemPrice.value;
+    td2.innerText = itemQuantity.value;
+    td3.innerText = totalPrice;
+
+    tr.appendChild(th);
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    tr.appendChild(td3);
+    infoTable.appendChild(tr);
 
 });
